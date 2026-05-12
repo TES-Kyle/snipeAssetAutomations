698b5212e79d057a863170a30cd1e92c48f1ccfe
ContractorJohnLockwood <johnlockwood@contractor.trinityes.org>
2026-05-12 09:24:04 -0400

    fixed very broken sync tasks

--- otherManagementFunctions/batteryDataSync.py
+++ otherManagementFunctions/batteryDataSync.py
@@ -1,3 +1,4 @@
 from utilities.windmill import run_windmill_job
 
-run_windmill_job("https://app.windmill.dev/api/w/trinity-it-dept-test-space/jobs/run/f/u/john/sync_battery_data_from_jamf_to_snipe_it")
+def run_jamf_battery_sync():
+    run_windmill_job("https://app.windmill.dev/api/w/trinity-it-dept-test-space/jobs/run/f/u/john/sync_battery_data_from_jamf_to_snipe_it")
--- otherManagementFunctions/jamfSync.py
+++ otherManagementFunctions/jamfSync.py
@@ -1,3 +1,4 @@
 from utilities.windmill import run_windmill_job
 
-run_windmill_job("https://app.windmill.dev/api/w/trinity-it-dept-test-space/jobs/run/f/u/john/asset_tag_sync")
+def run_snipe_to_jamf_sync():
+    run_windmill_job("https://app.windmill.dev/api/w/trinity-it-dept-test-space/jobs/run/f/u/john/asset_tag_sync")
